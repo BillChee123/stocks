3390ff488ec368c5a622dfab8c2fef5ae232b73b
BillChee123 <colo316@hotmail.com>
2020-06-14 15:32:36 +0800

    Set up mongoDB

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
 console.log("Hello World")
+
+const mongoDB = require('mongodb');
+
+const MongoClient = mongoDB.MongoClient;
+
+const uri = "mongodb+srv://billchee:<password>@cluster0-dq8sd.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("MongoDB Connected")
+  console.log(process.env.DOCKER_NETWORK)
+  client.close();
+});
